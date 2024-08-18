1fae3554bc5dafa41896083723fce45a7c617257
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-08-19 02:13:42 +0300

    fix route

--- Technicians_app/urls.py
+++ Technicians_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome,name="welcome"),  # Add this line
-    path('contact/',views.contact,name='contact'),  
-    path('login/', views.login,name='login'),
-    path('sign_up/', views.sign_up,name='sign_up'),
-    path('about_us/', views.about_us,name="about_us"),
+    path('contact',views.contact,name='contact'),  
+    path('login', views.login,name='login'),
+    path('sign_up', views.sign_up,name='sign_up'),
+    path('about_us', views.about_us,name="about_us"),
 ]
 
