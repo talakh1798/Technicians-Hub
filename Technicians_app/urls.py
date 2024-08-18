33bfa5c533ccfde63432052b5e05d599769857e1
Izzsamara <izzidinsamara@gmail.com>
2024-08-18 17:58:55 +0300

    update about us and urls

--- Technicians_app/urls.py
+++ Technicians_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome,name="welcome"),  # Add this line
-    path('about_us', views.about_us,name="about_us"),
+    path('about_us/', views.about_us,name="about_us"),
 ]
