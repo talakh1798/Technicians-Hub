5373906da7144718edf0fa527de9b618ddfc4ab6
Izzsamara <izzidinsamara@gmail.com>
2024-08-24 12:36:12 +0300

    Move video selection logic to app.js

--- Technicians_app/static/js/app.js
+++ Technicians_app/static/js/app.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
+
+  $(document).ready(function() {
+    // List of video URLs
+    var videoURLs = [
+        "https://cdn-static.findly.com/wp-content/uploads/sites/3285/2024/04/23090604/path-to-pro_intro-video.mp4",
+        "https://cdn-static.findly.com/wp-content/uploads/sites/3285/2024/04/23090628/path-to-pro_intro-video-2.mp4",
+        "https://cdn-static.findly.com/wp-content/uploads/sites/3285/2024/04/23090702/path-to-pro_intro-video-3.mp4"
+    ];
+
+    // Choose a random video URL
+    var randomVideoURL = videoURLs[Math.floor(Math.random() * videoURLs.length)];
+
+    // Generate video element and append it to .fullwidth-video div
+    var videoElement = '<video preload="auto" autoplay playsinline loop muted><source src="' + randomVideoURL + '" type="video/mp4"></video>';
+    $('.fullwidth-video').html(videoElement);
+});
