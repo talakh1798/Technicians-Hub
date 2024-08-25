ec89e0e311cb0d6b30bb29e4b7bbe622e48d50e7
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-08-25 20:04:45 +0300

    add

--- Technicians_project/settings.py
+++ Technicians_project/settings.py
@@ -48,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
 
 ROOT_URLCONF = 'Technicians_project.urls'
 
