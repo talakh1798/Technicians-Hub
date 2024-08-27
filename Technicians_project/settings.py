62fbd196f419765de2bf84685348a9ca6bea6d93
Izzsamara <izzidinsamara@gmail.com>
2024-08-27 19:15:47 +0300

    Switch database from SQLite to MySQL

--- Technicians_project/settings.py
+++ Technicians_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Technicians_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Tech_Hub_database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  
+        'PORT': '3306',       
     }
 }
 
