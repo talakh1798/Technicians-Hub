37a30729c7ba82e740ceacd400fa9f9a99deea3f
Izzsamara <izzidinsamara@gmail.com>
2024-10-27 21:44:25 +0200

    Updated Jazzmin settings: refined search model, sidebar icons, and removed unused configurations

--- Technicians_project/settings.py
+++ Technicians_project/settings.py
@@ -175,7 +175,7 @@ JAZZMIN_SETTINGS = {
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string 
-    "search_model": ["auth.User", "myapp.Techncians"],
+    "search_model": "auth.User",
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
@@ -216,18 +216,12 @@ JAZZMIN_SETTINGS = {
     # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
 
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": ["auth","myapp.park", "myapp.reservation","myapp.Comment" ],
-
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
-        "myapp.Park": "fas fa-parking",
-        "myapp.Reservation": "far fa-calendar",
-        "myapp.Comment": "far fa-comment",
     },
 
     # Icons that are used when one is not manually specified
